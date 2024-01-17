c49012972264e5fc0feead3483cc6daf2f66760d
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-17 11:36:37 +0500

    Add lucide-react dependency and update App component

--- youtube-design/src/App.tsx
+++ youtube-design/src/App.tsx
@@ -1,12 +1,12 @@
+import { PageHeader } from "./PageHeader"
 
-import './App.css'
+export default function App() {
+  return <div className="max-h-screen flex flex-col">
+    <PageHeader />
+    <div>2</div>
 
-export function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  </div>
 }
 
-export default App
+
+export { App }
