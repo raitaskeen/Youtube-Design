a95cab2a4c7d2a8263c98389addc9cdf63420c39
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-17 15:16:24 +0500

    Add search functionality and microphone button to PageHeader component

--- youtube-design/src/PageHeader.tsx
+++ youtube-design/src/PageHeader.tsx
@@ -1,8 +1,9 @@
-import { BellIcon, MenuIcon, UploadIcon, User } from 'lucide-react';
+import { BellIcon, MenuIcon, Mic, MicIcon, Search, UploadIcon, User,  } from 'lucide-react';
 import logo from '../src/assets/youtube.png';
 import { Button } from './components/Button';
 
 export function PageHeader() {
+    const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
   return <div className="flex gap-10 lg:gap-20 justify-between p-2 mb-6 mx-4">
     <div className="flex gap-4 items-center flex-shrink-0">
         <Button variant="ghost" size="icon" >
@@ -12,8 +13,24 @@ export function PageHeader() {
             <img src={logo}  className='h-5'/>
         </a>
     </div>
-    <div></div>
-    <div className='flex flex-shrink-0 md:gap-2'>
+    <form className='hidden  gap-4 flex-grow justify-center md:flex'>
+        <div className='flex flex-grow max-w-[600px]'>
+            <input type='search' placeholder='Search' className='rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none'/>
+            <Button className='py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0'>
+                <Search />
+            </Button>
+        </div>
+        <Button size={'icon'} type='button' className='flex-shrink-0'>
+            <MicIcon />
+        </Button>
+    </form>
+    <div className={'flex flex-shrink-0 md:gap-2 ${showFullWidthSearch ? hidden : flex}'}>
+    <Button onClick={() => setShowFullWidthSearch(true)} size={"icon"} variant={"ghost"} className='md:hidden'>
+            <Search  />
+        </Button>
+        <Button size={"icon"} variant={"ghost"} className='md:hidden'>
+            <Mic  />
+        </Button>
         <Button size={"icon"} variant={"ghost"}>
             <UploadIcon  />
         </Button>
