ed34ee4d09dbd496191a786ee90d5ea6db86ce8d
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-17 11:39:59 +0500

    Add MenuIcon to PageHeader component

--- youtube-design/src/PageHeader.tsx
+++ youtube-design/src/PageHeader.tsx
@@ -1,10 +1,11 @@
+import { MenuIcon } from 'lucide-react';
 import logo from '../src/assets/Logo.png';
 
 export function PageHeader() {
   return <div className="flex gap-10 lg:gap-20 justify-between">
     <div className="flex gap-4 items-center flex-shrink-0">
         <button>
-            <Menu />
+            <MenuIcon size={24} />
         </button>
         <a href="">
             <img src={logo}  className='h-6'/>
