e1174dc08003a42ce642cc3600e0f54f8afd63ec
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-17 10:52:38 +0500

    Update TailwindCSS configuration and build script

--- youtube-design/tailwind.config.js
+++ youtube-design/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    
+  ],
   theme: {
     extend: {},
   },
