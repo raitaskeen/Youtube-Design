8d98393e43f088217304308dbae2bd028233d181
Taskeen <raitaskeenhaider786@gmail.com>
2024-01-17 13:49:12 +0500

    Add new dependencies and update styles and components

--- youtube-design/src/PageHeader.tsx
+++ youtube-design/src/PageHeader.tsx
@@ -1,18 +1,29 @@
-import { MenuIcon } from 'lucide-react';
-import logo from '../src/assets/Logo.png';
+import { BellIcon, MenuIcon, UploadIcon, User } from 'lucide-react';
+import logo from '../src/assets/youtube.png';
+import { Button } from './components/Button';
 
 export function PageHeader() {
-  return <div className="flex gap-10 lg:gap-20 justify-between">
+  return <div className="flex gap-10 lg:gap-20 justify-between p-2 mb-6 mx-4">
     <div className="flex gap-4 items-center flex-shrink-0">
-        <button>
+        <Button variant="ghost" size="icon" >
             <MenuIcon size={24} />
-        </button>
+        </Button>
         <a href="">
-            <img src={logo}  className='h-6'/>
+            <img src={logo}  className='h-5'/>
         </a>
     </div>
     <div></div>
-    <div></div>
+    <div className='flex flex-shrink-0 md:gap-2'>
+        <Button size={"icon"} variant={"ghost"}>
+            <UploadIcon  />
+        </Button>
+        <Button size={"icon"} variant={"ghost"}>
+            <BellIcon  />
+        </Button>
+        <Button size={"icon"} variant={"ghost"}>
+            <User  />
+        </Button>
+    </div>
 
   </div>
 }
--- youtube-design/tailwind.config.js
+++ youtube-design/tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,7 +11,18 @@ export default {
     
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: {
+          DEFAULT: colors.neutral[200],
+          hover: colors.neutral[300],
+          border: colors.neutral[400],
+          text: colors.neutral[500],
+          dark: colors.neutral[800],
+          ["dark-hover"]: colors.neutral[900],
+        }
+      }
+    },
   },
   plugins: [],
 }
